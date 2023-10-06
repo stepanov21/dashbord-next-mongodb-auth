cef1593e3adfaa83e5ab708e8f8f04af6fd27b6d
Igor Stepanov <stepanovigor2110@gmail.com>
2023-10-06 15:05:31 +0300

    change ssr

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,34 +1,38 @@
+"use client";
+
 import FormAddProduct from "@/components/FormAddProduct/FormAddProduct";
 import ProductItem from "@/components/ProductItem/ProductItem";
 import { TProduct } from "@/models/product/index";
-import React from "react";
-
-async function getAllProducts() {
-  try {
-    const res = await fetch(
-      "http://localhost:3000/api/product/get-all-product",
-      {
-        method: "GET",
-        cache: "no-store",
-      }
-    );
-
-    const data = await res.json();
-
-    return data;
-  } catch (error) {
-    console.log("🚀 ~ file: page.tsx:18 ~ getAllProducts ~ error:", error);
-  }
-}
+import React, { useEffect, useState } from "react";
 
 export default async function ProductsPage() {
-  const { data: allProduct }: { data: TProduct[] } = await getAllProducts();
+  const [products, setProducts] = useState<TProduct[]>([]);
+
+  useEffect(() => {
+    async function getAllProducts() {
+      try {
+        const res = await fetch(
+          "http://localhost:3000/api/product/get-all-product",
+          {
+            method: "GET",
+            cache: "no-store",
+          }
+        );
+
+        const { data } = await res.json();
+        setProducts(data);
+      } catch (error) {
+        console.log("🚀 ~ file: page.tsx:18 ~ getAllProducts ~ error:", error);
+      }
+    }
+    getAllProducts();
+  }, []);
 
   return (
     <div className="p-6">
       <FormAddProduct />
-      {allProduct &&
-        allProduct.map((product, key) => {
+      {products &&
+        products.map((product, key) => {
           return <ProductItem key={key} {...product} />;
         })}
     </div>
