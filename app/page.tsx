40075196e059683e386eeec51a9825aa282c5656
Igor Stepanov <stepanovigor2110@gmail.com>
2023-10-06 15:08:12 +0300

    add ssr

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,5 @@
-'use client'
-
-import Chart from "react-apexcharts";
+"use client";
 
 export default function Home() {
-  const obj = {
-    options: {
-      chart: {
-        id: "basic-bar",
-      },
-      xaxis: {
-        categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999],
-      },
-    },
-    series: [
-      {
-        name: "series-1",
-        data: [30, 40, 45, 50, 49, 60, 70, 91],
-      },
-    ],
-  };
-  return (
-    <section>
-      <Chart options={obj.options} series={obj.series} width="500" />
-    </section>
-  );
+  return <section></section>;
 }
