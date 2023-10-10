fa270082ad4c0fd0186ae97906416718a93d07d1
Igor Stepanov <stepanovigor2110@gmail.com>
2023-10-10 16:26:23 +0300

    mvp 2

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -5,10 +5,14 @@ import { headers } from "next/headers";
 
 async function getAllProducts() {
   try {
-    const res = await fetch(`/api/product/get-all-product`, {
-      method: "GET",
-      cache: "no-store",
-    });
+    const res = await fetch(
+      `${process.env.NEXTAUTH_URL}/api/product/get-all-product`,
+      {
+        method: "GET",
+        cache: "no-store",
+        headers: headers(),
+      }
+    );
     const { data } = await res.json();
 
     return data;
