1a4b3464039d8d237ba8ee36426cd2e9dc9e468b
Igor Stepanov <stepanovigor2110@gmail.com>
2023-10-10 20:40:24 +0300

    back to isr

--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,63 @@
-import { headers } from "next/headers";
+"use client";
+
 import { TProduct } from "@/models/product/index";
-import Card from "@/components/Card/Card";
+import { selectOption } from "@/components/FormControls/selectOption";
+import { useEffect, useState } from "react";
 
-export const dynamic = "force-dynamic";
+const Home = () => {
+  const [data, setData] = useState<TProduct[]>([]);
 
-async function getAllMyProducts() {
-  try {
-    const res = await fetch(`/api/product/get-all-product`, {
-      method: "GET",
-      cache: "no-store",
-      headers: headers(),
-    });
-    const { data } = await res.json();
+  useEffect(() => {
+    async function getAllMyProducts() {
+      try {
+        const res = await fetch(
+          `${process.env.NEXTAUTH_URL}/api/product/get-all-product`,
+          {
+            method: "GET",
+            cache: "no-store",
+          }
+        );
+        const { data } = await res.json();
 
-    return data;
-  } catch (error) {
-    console.log("🚀 ~ file: page.tsx:18 ~ getAllProducts ~ error:", error);
-  }
-}
+        return data;
+      } catch (error) {
+        console.log("🚀 ~ file: page.tsx:18 ~ getAllProducts ~ error:", error);
+      }
+    }
+    getAllMyProducts().then((res) => setData(res));
+  }, []);
 
-export default async function Home() {
-  const data = await getAllMyProducts();
-
-  function getAccumFromCategory(data: TProduct[], filter: string) {
+  const getAccumFromCategory = (data: TProduct[], filter: string) => {
     const category = data.filter((item) =>
       item.category === filter ? item : null
     );
     return category.reduce((a, i) => a + i.price, 0);
-  }
-
-  const sum = getAccumFromCategory(data, "Продукты");
+  };
 
   console.log(
     "🚀 ~ file: page.tsx:28 ~ getAccumFromCategory ~ getAccumFromCategory:",
     getAccumFromCategory(data, "Продукты")
   );
-  return <div>{/* <Card filter="Продукты" sum={sum} /> */}</div>;
-}
+
+  const state = {
+    options: {
+      chart: {
+        id: "bar",
+      },
+      xaxis: {
+        categories: [...selectOption],
+      },
+    },
+    series: [
+      {
+        name: "series-1",
+        data: selectOption.map((item, i) =>
+          getAccumFromCategory(data, selectOption[i])
+        ),
+      },
+    ],
+  };
+  return <div></div>;
+};
+
+export default Home;
--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,35 +1,42 @@
+"use client";
+
 import FormAddProduct from "@/components/FormAddProduct/FormAddProduct";
 import ProductItem from "@/components/ProductItem/ProductItem";
 import { TProduct } from "@/models/product/index";
 import { headers } from "next/headers";
+import { useEffect, useState } from "react";
 
-export const dynamic = "force-dynamic";
+const ProductPage = () => {
+  const [data, setData] = useState<TProduct[]>([]);
 
-async function getAllProducts() {
-  try {
-    const res = await fetch(`/api/product/get-all-product`, {
-      method: "GET",
-      cache: "no-store",
-      headers: headers(),
-    });
-    const { data } = await res.json();
+  useEffect(() => {
+    async function getAllMyProducts() {
+      try {
+        const res = await fetch(
+          `${process.env.NEXTAUTH_URL}/api/product/get-all-product`,
+          {
+            method: "GET",
+            cache: "no-store",
+          }
+        );
+        const { data } = await res.json();
 
-    return data;
-  } catch (error) {
-    console.log("🚀 ~ file: page.tsx:18 ~ getAllProducts ~ error:", error);
-  }
-}
-
-export default async function ProductPage() {
-  const products: TProduct[] = await getAllProducts();
-  console.log(products);
+        return data;
+      } catch (error) {
+        console.log("🚀 ~ file: page.tsx:18 ~ getAllProducts ~ error:", error);
+      }
+    }
+    getAllMyProducts().then((res) => setData(res));
+  }, []);
   return (
     <div className="">
       <FormAddProduct />
-      {products &&
-        products.map((product, key) => {
+      {data &&
+        data.map((product, key) => {
           return <ProductItem key={key} {...product} _id={product._id} />;
         })}
     </div>
   );
-}
+};
+
+export default ProductPage;
