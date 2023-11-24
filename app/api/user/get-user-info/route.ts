72cea7fb3c943471be11a405769c54e0fa7a7ef8
Igor Stepanov <stepanovigor2110@gmail.com>
2023-11-24 21:00:45 +0200

    add force-dynamic

--- app/api/user/get-user-info/route.ts
+++ app/api/user/get-user-info/route.ts
@@ -4,6 +4,7 @@ import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server"
 import { authOptions } from "../../auth/[...nextauth]/route";
 
+export const dynamic = 'force-dynamic'
 
 export async function GET(req: NextRequest) {
   try{
